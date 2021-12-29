75b0031544046bb4461dd8a32cb3f768a330bc50
obviouswhy <sergilockerz@gmail.com>
2021-12-29 11:17:53 +0100

    added album cover + title - subtitle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className={'App'}>
+      <section className={'album-wrapper'}>
+        <img
+          alt={'Album Cover'}
+          src="https://d1iudujjh4zmgc.cloudfront.net/uploads/image/asset/503/medium_af262904-5003-49d4-a1a1-d193c3de4c62.jpg"
+        />
+        <div className={'title-wrapper'}>
+          <h3>BLADEE - 333</h3>
+          <h4>CHOOSE MUSIC SERVICE</h4>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default App
